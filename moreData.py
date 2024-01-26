fadf8940679c915579a131132da64d4ee024c6fa
francsir <72939196+francsir@users.noreply.github.com>
2024-01-26 14:19:57 +0000

    moreData finished for example

--- moreData.py
+++ moreData.py
@@ -1,4 +1,29 @@
 from datasets import load_dataset
+from transformers import AutoTokenizer, DataCollatorWithPadding
 
+##loading the mrpc dataset from the glue tasks, will be split into train val and test
 raw_dataset = load_dataset("glue", "mrpc")
-print(raw_dataset)
+
+# Take the train data set for inspection
+raw_train_dataset = raw_dataset["train"]
+print(raw_train_dataset[0])
+
+##find the features of the dataset, and what they mean
+print(raw_train_dataset.features)
+
+checkpoint = "bert-base-uncased"
+tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+## function that will tokenize the data and keep is as a dataset, 
+def tokenize_function(example):
+    return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+tokenized_datasets = raw_dataset.map(tokenize_function, batched=True)
+data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+sample = tokenized_datasets["train"][:8]
+sample = {k: v for k, v in sample.items() if k not in ['idx', 'sentence1', 'sentence2']}
+
+batch = data_collator(sample)
+print({k:v.shape for k, v in batch.items()})
