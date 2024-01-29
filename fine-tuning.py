e12aaf3096af661f5505792a12b985bb328c05fc
francsir <72939196+francsir@users.noreply.github.com>
2024-01-29 16:41:38 +0000

    more fine tunning

--- fine-tuning.py
+++ fine-tuning.py
@@ -1,4 +1,4 @@
-from transformers import TrainingArguments, AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding
+from transformers import Trainer, TrainingArguments, AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding
 from datasets import load_dataset
 
 ##define the trainging arguments, test_trainer is the name of the folder where the model will be saved
@@ -16,3 +16,14 @@ tokenized_datasets = raw_dataset.map(tokenize_function, batched=True)
 data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
 
 model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+
+trainer = Trainer(
+    model,
+    training_args,
+    train_dataset=tokenized_datasets["train"],
+    eval_dataset=tokenized_datasets["validation"],
+    data_collator=data_collator,
+    tokenizer=tokenizer,
+)
+
+trainer.train()
