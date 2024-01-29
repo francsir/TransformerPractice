ec0d0a3e76ebe27a82be27c7f557d99533e8cb9d
francsir <72939196+francsir@users.noreply.github.com>
2024-01-29 22:01:30 +0000

    fine tuning with evaluate complete

--- fine-tuning.py
+++ fine-tuning.py
@@ -1,5 +1,7 @@
 from transformers import Trainer, TrainingArguments, AutoModelForSequenceClassification, AutoTokenizer, DataCollatorWithPadding
 from datasets import load_dataset
+import evaluate
+import numpy as np
 
 ##define the trainging arguments, test_trainer is the name of the folder where the model will be saved
 training_args = TrainingArguments("test_trainer")
@@ -8,6 +10,14 @@ training_args = TrainingArguments("test_trainer")
 raw_dataset = load_dataset("glue", "mrpc")
 checkpoint = "bert-base-uncased"
 tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+
+def compute_metrics(eval_preds):
+    metric = evaluate.load("glue", "mrpc")
+    logits, labels = eval_preds
+    predictions = np.argmax(logits, axis=-1)
+    return metric.compute(predictions=predictions, references=labels)
+
 
 def tokenize_function(example):
     return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
@@ -24,6 +34,10 @@ trainer = Trainer(
     eval_dataset=tokenized_datasets["validation"],
     data_collator=data_collator,
     tokenizer=tokenizer,
+    compute_metrics=compute_metrics,
 )
 
 trainer.train()
+
+preditctions = trainer.predict(tokenized_datasets["validation"])
+print(preditctions.predictions.shape, preditctions.label_ids.shape)
